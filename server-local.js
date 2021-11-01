9101ca5abafae5a145e027b6719b6ec58684f509
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2021-11-01 17:26:07 +0700

    update server-local.js

--- server-local.js
+++ server-local.js
@@ -2,4 +2,4 @@
 
 const app = require('./express/event-gif');
 
-app.listen(9000, () => console.log('Local app listening on port 9000!'));
+app.listen(80, () => console.log('Local app listening on port 80!'));
