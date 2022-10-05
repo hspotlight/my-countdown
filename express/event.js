6c1a4e97c1fc46c8d1095ee826cb60047ec97de8
Pakawat Anekwiroj <pakawat.ane@gmail.com>
2022-10-05 14:27:23 +0700

    extract constants

--- express/event.js
+++ express/event.js
@@ -1,3 +1,9 @@
+const SECOND_IN_MS = 1000;
+const MINUTE_IN_MS = SECOND_IN_MS * 60;
+const HOUR_IN_MS = MINUTE_IN_MS * 60;
+const DAY_IN_MS = HOUR_IN_MS * 24;
+const WIDTH = 320;
+const HEIGHT = 240;
 /* eslint-disable no-unused-vars */
 /* eslint-disable spaced-comment */
 const GIFEncoder = require("gifencoder");
@@ -30,7 +36,7 @@ function eventGif(fs, endDate, eventName, padString, startDate) {
   //console.log("total seconds"+numberOfSeconds);
   //console.log("total numberOfSecondsEnd"+endNumberOfSeconds);
 
-  const encoder = new GIFEncoder(320, 240);
+  const encoder = new GIFEncoder(WIDTH, HEIGHT);
   // stream the results as they are available into myanimated.gif
   encoder.createReadStream().pipe(fs);
 
@@ -40,7 +46,7 @@ function eventGif(fs, endDate, eventName, padString, startDate) {
   encoder.setQuality(10); // image quality. 10 is default.
 
   // use node-canvas
-  const canvas = createCanvas(320, 240);
+  const canvas = createCanvas(WIDTH, HEIGHT);
   const ctx = canvas.getContext("2d");
   let blink = 0;
   for (
@@ -50,16 +56,16 @@ function eventGif(fs, endDate, eventName, padString, startDate) {
   ) {
     //console.log("render"+distance);
     // Time calculations for days, hours, minutes and seconds
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const days = Math.floor(distance / DAY_IN_MS);
+    const hours = Math.floor((distance % DAY_IN_MS) / HOUR_IN_MS);
+    const minutes = Math.floor((distance % HOUR_IN_MS) / MINUTE_IN_MS);
+    const seconds = Math.floor((distance % MINUTE_IN_MS) / SECOND_IN_MS);
 
     //console.log(`d HH mm ss ${days} ${hours} ${minutes} ${seconds}`);
 
     // blue rectangle
     ctx.fillStyle = "#0000ff";
-    ctx.fillRect(0, 0, 320, 240);
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
     ctx.fillStyle = "#ffffff";
     ctx.font = "30px Impact";
