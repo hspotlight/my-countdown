046c331a98152c6d3401010f1afc8a97f32550b5
Pakawat Anekwiroj <pakawat.ane@gmail.com>
2022-10-05 14:21:10 +0700

    extract currentYear var

--- express/event-gif.js
+++ express/event-gif.js
@@ -7,15 +7,19 @@ const { eventGif } = require("./event");
 const app = express();
 const router = express.Router();
 
+const currentYear = "2022";
+
 router.get("/", (req, res) => {
   // Access the provided 'page' and 'limt' query parameters
 
   const eventName = req.query.text
     ? req.query.text
-    : "Hacktoberfest Open Hack Day Bangkok 2021";
+    : `Hacktoberfest Open Hack Day Bangkok ${currentYear}`;
   const padString = req.query.padding ? req.query.padding : "0";
   const startDate = new Date().getTime();
-  const eventDate = req.query.on ? req.query.on : "Oct 30, 2021 09:00:00";
+  const eventDate = req.query.on
+    ? req.query.on
+    : `Oct 30, ${currentYear} 09:00:00`;
   const endDate = new Date(eventDate).getTime();
   res.set("accept-ranges", "bytes;");
   res.set("Content-Disposition", "inline;");
@@ -28,7 +32,9 @@ router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use("/.netlify/functions/event-gif", router); // path must route to lambda
-app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
+app.use("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+);
 
 module.exports = app;
 module.exports.handler = serverless(app);
